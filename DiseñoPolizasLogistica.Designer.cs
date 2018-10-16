3db1d32d46bf6a8302e09b5a91cac6208e7588f0
ValeryAS2 <41198395+ValeryAS2@users.noreply.github.com>
2018-10-15 22:39:32 -0600

    Add files via upload

---
+++ DiseñoPolizasLogistica.Designer.cs
@@ -0,0 +1,309 @@
+﻿namespace CapaDeDiseñoPolizasLogistica
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.Lbl_nombreForm = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.navegador1 = new CapaDiseno.Navegador();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.Btn_poliza = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.Btn_Cerrar = new System.Windows.Forms.Button();
+            this.Btn_Minimizar = new System.Windows.Forms.Button();
+            this.Poliza = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Concepto = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.NombreCuenta = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Debe = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Haber = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Tipo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Total = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(108)))), ((int)(((byte)(154)))), ((int)(((byte)(152)))));
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Controls.Add(this.Lbl_nombreForm);
+            this.panel1.Controls.Add(this.Btn_Cerrar);
+            this.panel1.Controls.Add(this.Btn_Minimizar);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(933, 42);
+            this.panel1.TabIndex = 0;
+            // 
+            // Lbl_nombreForm
+            // 
+            this.Lbl_nombreForm.AutoSize = true;
+            this.Lbl_nombreForm.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Lbl_nombreForm.Location = new System.Drawing.Point(52, 7);
+            this.Lbl_nombreForm.Name = "Lbl_nombreForm";
+            this.Lbl_nombreForm.Size = new System.Drawing.Size(259, 28);
+            this.Lbl_nombreForm.TabIndex = 2;
+            this.Lbl_nombreForm.Text = "5401_Polizas logistica";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(213, 136);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(325, 26);
+            this.dateTimePicker1.TabIndex = 1;
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Location = new System.Drawing.Point(213, 184);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(325, 26);
+            this.dateTimePicker2.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(32, 139);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(162, 23);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Fecha de inicio";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(32, 187);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(136, 23);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Fecha de Fin";
+            // 
+            // navegador1
+            // 
+            this.navegador1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.navegador1.DataGr = null;
+            this.navegador1.Forma = null;
+            this.navegador1.Location = new System.Drawing.Point(5, 52);
+            this.navegador1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.navegador1.Name = "navegador1";
+            this.navegador1.Procedimiento = null;
+            this.navegador1.Size = new System.Drawing.Size(1239, 66);
+            this.navegador1.TabIndex = 5;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToOrderColumns = true;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.ColumnHeader;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ActiveBorder;
+            this.dataGridView1.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.Raised;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Poliza,
+            this.Concepto,
+            this.NombreCuenta,
+            this.Debe,
+            this.Haber,
+            this.Tipo,
+            this.Total});
+            this.dataGridView1.GridColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.dataGridView1.Location = new System.Drawing.Point(49, 301);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(725, 304);
+            this.dataGridView1.TabIndex = 7;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(32, 229);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 23);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Area";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Inventarios",
+            "Compras"});
+            this.comboBox1.Location = new System.Drawing.Point(213, 224);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(148, 28);
+            this.comboBox1.TabIndex = 9;
+            // 
+            // Btn_poliza
+            // 
+            this.Btn_poliza.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(241)))), ((int)(((byte)(255)))));
+            this.Btn_poliza.BackgroundImage = global::CapaDeDiseñoPolizasLogistica.Properties.Resources.archivo;
+            this.Btn_poliza.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.Btn_poliza.Location = new System.Drawing.Point(576, 139);
+            this.Btn_poliza.Name = "Btn_poliza";
+            this.Btn_poliza.Size = new System.Drawing.Size(65, 71);
+            this.Btn_poliza.TabIndex = 6;
+            this.Btn_poliza.Text = "Poliza";
+            this.Btn_poliza.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.Btn_poliza.UseVisualStyleBackColor = false;
+            // 
+            // panel2
+            // 
+            this.panel2.BackgroundImage = global::CapaDeDiseñoPolizasLogistica.Properties.Resources.transportadora1;
+            this.panel2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.panel2.Location = new System.Drawing.Point(5, 1);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(41, 41);
+            this.panel2.TabIndex = 1;
+            // 
+            // Btn_Cerrar
+            // 
+            this.Btn_Cerrar.BackgroundImage = global::CapaDeDiseñoPolizasLogistica.Properties.Resources.Cancelar;
+            this.Btn_Cerrar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.Btn_Cerrar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Btn_Cerrar.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(108)))), ((int)(((byte)(154)))), ((int)(((byte)(152)))));
+            this.Btn_Cerrar.Location = new System.Drawing.Point(887, 2);
+            this.Btn_Cerrar.Name = "Btn_Cerrar";
+            this.Btn_Cerrar.Size = new System.Drawing.Size(41, 41);
+            this.Btn_Cerrar.TabIndex = 1;
+            this.Btn_Cerrar.UseVisualStyleBackColor = true;
+            // 
+            // Btn_Minimizar
+            // 
+            this.Btn_Minimizar.BackgroundImage = global::CapaDeDiseñoPolizasLogistica.Properties.Resources.minimize;
+            this.Btn_Minimizar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.Btn_Minimizar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Btn_Minimizar.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(108)))), ((int)(((byte)(154)))), ((int)(((byte)(152)))));
+            this.Btn_Minimizar.Location = new System.Drawing.Point(831, 1);
+            this.Btn_Minimizar.Name = "Btn_Minimizar";
+            this.Btn_Minimizar.Size = new System.Drawing.Size(41, 41);
+            this.Btn_Minimizar.TabIndex = 0;
+            this.Btn_Minimizar.UseVisualStyleBackColor = true;
+            // 
+            // Poliza
+            // 
+            this.Poliza.HeaderText = "Poliza";
+            this.Poliza.Name = "Poliza";
+            this.Poliza.Width = 87;
+            // 
+            // Concepto
+            // 
+            this.Concepto.HeaderText = "Concepto";
+            this.Concepto.Name = "Concepto";
+            this.Concepto.Width = 114;
+            // 
+            // NombreCuenta
+            // 
+            this.NombreCuenta.HeaderText = "Nombre Cuentas";
+            this.NombreCuenta.Name = "NombreCuenta";
+            this.NombreCuenta.Width = 152;
+            // 
+            // Debe
+            // 
+            this.Debe.HeaderText = "Debe";
+            this.Debe.Name = "Debe";
+            this.Debe.Width = 84;
+            // 
+            // Haber
+            // 
+            this.Haber.HeaderText = "Haber";
+            this.Haber.Name = "Haber";
+            this.Haber.Width = 89;
+            // 
+            // Tipo
+            // 
+            this.Tipo.HeaderText = "Tipo";
+            this.Tipo.Name = "Tipo";
+            this.Tipo.Width = 75;
+            // 
+            // Total
+            // 
+            this.Total.HeaderText = "Total";
+            this.Total.Name = "Total";
+            this.Total.Width = 80;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(130)))), ((int)(((byte)(168)))), ((int)(((byte)(167)))));
+            this.ClientSize = new System.Drawing.Size(933, 700);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.Btn_poliza);
+            this.Controls.Add(this.navegador1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button Btn_Cerrar;
+        private System.Windows.Forms.Button Btn_Minimizar;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label Lbl_nombreForm;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private CapaDiseno.Navegador navegador1;
+        private System.Windows.Forms.Button Btn_poliza;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Poliza;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Concepto;
+        private System.Windows.Forms.DataGridViewTextBoxColumn NombreCuenta;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Debe;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Haber;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Tipo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Total;
+    }
+}
